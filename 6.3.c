360645de65b6c3e3e064f745b45b350bf96ebeb0
Kumarjeet Ray <53223216+kumarjeetray@users.noreply.github.com>
2019-08-18 15:16:21 +0530

    Add files via upload

---
+++ 6.3.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+    int a,b,c,d,m,n;
+    float x1=0.0,x2=0.0;
+    printf("Enter a:");
+    scanf("%d",&a);
+    printf("\nEnter b:");
+    scanf("%d",&b);
+    printf("\nEnter c:");
+    scanf("%d",&c);
+    printf("\nEnter d:");
+    scanf("%d",&d);
+    printf("\nEnter m:");
+    scanf("%d",&m);
+    printf("ENter n:");
+    scanf("%d",&n);
+    if((a*d-c*b)==0)
+    {
+        printf("Denominator is Zero");
+        exit(0);
+    }
+    else
+    {
+        x1=(m*d-b*n)/(a*d-c*b);
+        x2=(n*a-m*c)/(a*d-c*b);
+    }
+    printf("\nX1=%f",x1);
+    printf("\nX2=%f",x2);
+}
+    
